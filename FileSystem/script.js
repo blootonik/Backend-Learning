e7efc9158a41f0ac88d9fb32943c4b4eae2290b3
blootonik <blootonik@gmail.com>
2024-08-14 09:02:33 +0530

    This Is My Learning for File system

---
+++ FileSystem/script.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+// fs.writeFile("firstBackendFile.txt","This is my first code in backend",(err)=>{
+//     if(err){console.log(err)}
+//     else{console.log("done")}
+// })
+
+// fs.appendFile("firstBackendFile.txt","And this is second code in Backend",(err)=>{
+//     if(err){console.log(err)}
+//     else{console.log("done2")}
+// })
+
+// fs.rename("firstBackendFile.txt","thirdCodeFile.txt",(err)=>{
+//     if(err){console.log(err)}
+//     else{console.log("done3")}
+// })
+
+// fs.copyFile("firstBackendFile.txt","./fourthCode/myFourthcode.txt",(err)=>{
+//     if(err){console.log(err)}
+//     else{console.log("fouth Done")}
+// })
+
+// fs.unlink("thirdCodeFile.txt",(err)=>{
+//     if(err){console.log(err)}
+//     else{console.log("fifth Done deletion !!")}
+// })
+
+// fs.rmdir("./fourthCode",{recursive:true},(err)=>{
+//     if(err){console.log(err)}
+//     else{console.log("folder Deleted")}
+// })
+fs.rm("./fourthCode",{recursive:true},(err)=>{
+        if(err){console.log(err)}
+        else{console.log("folder Deleted")}
+    })
+
