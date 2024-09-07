ede426763049c93806c324e62c871bb283ee967a
blootonik <blootonik@gmail.com>
2024-09-07 12:36:06 +0530

    Ejs & Dynamic Routing

---
+++ EJS-DynamicRouting/index.js
@@ -0,0 +1,20 @@
+// console.log(path.join(__dirname,'public'))
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
+app.set('view engine', 'ejs');
+
+app.get("/", function(req,res){
+    res.render("index");
+})
+app.get("/profile/:username/:age", function(req,res){
+    res.send(`Hell, ${req.params.username} of age ${req.params.age}`);
+})
+app.listen(3000,function(){
+    console.log("its Running");
+})
